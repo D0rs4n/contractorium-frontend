698ab37d21e5ffdd0e97e6055f96b9db34078791
Marci <mihmar17@gmail.com>
2023-01-24 21:02:16 +0100

    creator prop add

--- frontend/src/lib/collections.ts
+++ frontend/src/lib/collections.ts
@@ -1,12 +1,14 @@
 import type { ABIValue } from "algosdk";
 
 export class BugBounty {
+    creator: ABIValue;
     name: ABIValue;
     description: ABIValue;
     verified: ABIValue;
     image: ABIValue;
 
-    constructor(name: ABIValue, description: ABIValue, verified: ABIValue, image: ABIValue) {
+    constructor(creator: ABIValue,name: ABIValue, description: ABIValue, verified: ABIValue, image: ABIValue) {
+        this.creator = creator;
         this.name = name;
         this.description = description
         this.verified = verified
