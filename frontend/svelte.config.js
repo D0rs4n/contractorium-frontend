7426777fbd6ed90597ae68b820815ed7cfb37c4b
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2023-01-26 14:46:08 +0100

    Add additional XSS protection

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -8,7 +8,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		csp: {
+			directives: {
+			  'script-src': ['self'],
+			},
+		  },
 	}
 };
 
