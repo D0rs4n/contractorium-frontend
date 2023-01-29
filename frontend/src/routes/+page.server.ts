2e189ba2a249e83e60209b48709688f015f8d836
D0rs4n <41237606+D0rs4n@users.noreply.github.com>
2023-01-29 15:49:45 +0100

    Update form submission

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,21 +1,16 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import type { Actions } from './$types';
-import pinataSDK from '@pinata/sdk'
 import { env } from '$env/dynamic/private';
 
-const pinataClient = new pinataSDK(env.PRIVATE_PINATA_API_KEY, env.PRIVATE_PINATA_API_SECRET);
+
 const JWT = "Bearer " + env.PRIVATE_PINATA_JWT
+const IPFSGateway = "https://ipfs.algonode.xyz/ipfs/"
 export const actions = {
   editprogram: async ({ request }) => {
     const data = await request.formData();
     const image_file = data.get("image") as File  
-    try {
-      await pinataClient.testAuthentication()
-    }
-    catch (err) {
-      return { success: false }
-    }
     const formData: FormData = new FormData();
+    let ipfs_hash: string;
     formData.append('file', image_file)
     const metaData = JSON.stringify({
       name: image_file.name
@@ -25,27 +20,22 @@ export const actions = {
       cidVersion: 0,
     });
     formData.append('pinataOptions', options);
-
-
     try {
       const res = await fetch("https://api.pinata.cloud/pinning/pinFileToIPFS", {
         method: "POST",
         body: formData,
         headers: {
-          //@ts-ignore
-          'Content-Type': `multipart/form-data; boundary=${formData._boundary}`, 
-          //@ts-ignore
-          'Content-Length': formData.length,
           Authorization: JWT
         }
       });
       const json = await res.json();
-      console.log(json);
+      ipfs_hash = json["IpfsHash"]
+
     } catch (error) {
-      console.log(error);
+      return { success: false };
     }
 
-    return { success: false, edit: true,  data: { name: data.get("name")?.toString() || "", description: data.get("description")?.toString() || "", image: data.get("image")?.toString() || "" }}
+    return { success: true, edit: true,  data: { name: data.get("name")?.toString() || "", description: data.get("description")?.toString() || "", image: ipfs_hash || "" }}
     
   },
   newprogram: async ({ request }) => {
