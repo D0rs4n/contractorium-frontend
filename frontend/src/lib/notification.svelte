2fefa8d2faa592902309baa302c43903da57b756
Marci <mihmar17@gmail.com>
2023-02-01 22:23:25 +0100

    Add notification

---
+++ frontend/src/lib/notification.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import NotificationCard from './notificationCard.svelte';
+	import { notifications } from './store_bounty';
+</script>
+
+<div class="absolute z-50 w-full">
+	{#each $notifications as notification}
+		<NotificationCard {notification} />
+	{/each}
+</div>
--- frontend/src/lib/store_bounty.ts
+++ frontend/src/lib/store_bounty.ts
@@ -2,22 +2,41 @@ import { writable } from 'svelte/store';
 
 export const isShowing = writable<boolean>(false); // createBountyModal visibility
 
-
 // Form data
 interface newBountyValue {
-    name: string;
-    description: string;
-    image: string;
+	name: string;
+	description: string;
+	image: string;
 }
 
 export const newBountyValue = writable<newBountyValue>({
-    name: "",
-    description: "",
-    image: ""
-})
+	name: '',
+	description: '',
+	image: ''
+});
 //END
 
-
-export const trigger_new_bounty = writable<boolean>(true); // true if add new bounty, else 
+export const trigger_new_bounty = writable<boolean>(true); // true if add new bounty, else
 
 export const trigger = writable<boolean>(false); // trigger new bounty fetch
+
+//Notifications
+
+export interface Notification {
+	type: string;
+	title: string;
+	desc: string;
+}
+
+function createNotifications() {
+	const { subscribe, update } = writable<Notification[]>([]);
+
+	return {
+		subscribe,
+		add: (type: string, title: string, desc: string) => {
+			update((p) => [...p, { type, title, desc }]);
+		}
+	};
+}
+
+export const notifications = createNotifications();
